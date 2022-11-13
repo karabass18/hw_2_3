bed853f396e699d7338ad2f2f949e702353f69eb
Aleksei Dementev <karabass18@gmail.com>
2022-11-14 02:30:55 +0500

    added Muzzy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello karabass  ");
+        System.out.println("I am Muzzy!  ")
     }
 }
